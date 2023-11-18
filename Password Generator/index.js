c4611980889a6cb175d816e496267340e3158076
VishalPSIT <vishal.psit2213663@gmail.com>
2023-11-18 22:40:17 +0530

    adding js

---
+++ Password Generator/index.js
@@ -0,0 +1,14 @@
+const inputSlider = document.querySelector("[data-lengthSlider]");
+const lengthDisplay = document.querySelector("[data-length]");
+const passwordDisplay = document.querySelector("[data-passwordDisplay]");
+const copybtn = document.querySelector("[data-copy]");
+const copyMsg = document.querySelector("[data-copyMsg]");
+const upperCaseCheck = document.querySelector("#uppercase");
+const lowerCaseCheck = document.querySelector("#lowercase");
+const numberCheck = document.querySelector("#numbers");
+const symbolCheck = document.querySelector("#symbols");
+const indicator = document.querySelector("[data-indicator]");
+const generateBtn = document.querySelector("[data-createPassword]");
+const allCheckBox = document.querySelectorAll("input[type=checkbox]");
+
+
